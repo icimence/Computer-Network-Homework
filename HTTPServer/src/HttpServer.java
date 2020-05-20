3f018e0a5cd4a12fcd4a7e7b1e1b23a3bdf2ba00
allen <181250066@smail.nju.edu.cn>
2020-05-20 18:48:07 +0800

    fix: nullPointer Bugs

--- HTTPServer/src/HttpServer.java
+++ HTTPServer/src/HttpServer.java
@@ -1,11 +1,9 @@
-import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
 
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.io.*;
 import java.util.Map;
 import java.util.StringTokenizer;
-import java.util.function.DoubleToIntFunction;
 
 //实现Runnable接口
 public class HttpServer implements Runnable{
@@ -19,17 +17,16 @@ public class HttpServer implements Runnable{
         try {
             ServerSocket serverSocket = new ServerSocket(DEFAULT_PORT);
             System.out.println("Server is listening for a connection on port: "+DEFAULT_PORT+"......");
-            while(true){//不断监听客户端传来的消息，此过程中server始终开启
+            while(true){//通过死循环建立长连接，不断监听客户端传来的消息
                 Socket client = serverSocket.accept();//接收客户端的连接
                 System.out.println("Server connected.");
                 HttpServer httpServer = new HttpServer(client);
                 Thread thread = new Thread(httpServer);//为连接的客户端开一个线程
-                thread.start();
+                thread.start();//启动线程
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
         }
-
     }
 
     public void run(){
@@ -46,31 +43,44 @@ public class HttpServer implements Runnable{
             //数据部分
             outputStream = new BufferedOutputStream(socket.getOutputStream());
             //获取第一行
-            String firstLine = bufferedReader.readLine();
-            StringTokenizer tokenizer = new StringTokenizer(firstLine);
-            //获取方法名
-            String Method = tokenizer.nextToken().toUpperCase();
-            //获取url
-            String url = tokenizer.nextToken().toLowerCase();
-            //该服务器只支持GET和POST方法
-            if(Method.equals("GET")){
-                //向客户端传送header
-                writer.println("HTTP/1.1 200 OK");
-                writer.println("Server: HttpServer");
-                writer.println("Accept: */*");
-                writer.println("Accept-language: zh-cn");
-                writer.println("Connection: keep-alive");
-                writer.println("Host: localhost");
-                writer.println();
-                writer.flush();
-                //额...暂时没写数据
-                outputStream.flush();
-            }
-            else if(Method.equals("POST")){
-                //TODO
-            }
-            else{
-                System.out.println("Method not supported.");
+            try {
+                String firstLine = bufferedReader.readLine();
+                StringTokenizer tokenizer = new StringTokenizer(firstLine);
+                //获取方法名
+                String Method = tokenizer.nextToken().toUpperCase();
+                //获取url
+                String url = tokenizer.nextToken().toLowerCase();
+                //该服务器只支持GET和POST方法
+                if (Method.equals("GET")) {
+                    //向客户端传送header
+                    writer.println("HTTP/1.1 200 OK");
+                    writer.println("Server: HttpServer");
+                    writer.println("Accept: */*");
+                    writer.println("Accept-language: zh-cn");
+                    writer.println("Connection: keep-alive");
+                    writer.println("Host: localhost");
+                    writer.println();
+                    writer.flush();
+                    //额...暂时没写数据
+                    outputStream.flush();
+                } else if (Method.equals("POST")) {
+                    //TODO
+                    writer.println("HTTP/1.1 200 OK");
+                    writer.println("Server: HttpServer");
+                    writer.println("Accept: */*");
+                    writer.println("Accept-language: zh-cn");
+                    writer.println("Connection: keep-alive");
+                    writer.println("Host: localhost");
+                    writer.println();
+                    writer.flush();
+                    //额...暂时没写数据
+                    outputStream.write("Hello World!".getBytes());
+                    outputStream.flush();
+                } else {
+                    System.out.println("Method not supported.");
+                }
+            }catch(NullPointerException npe){
+                npe.getMessage();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -85,8 +95,5 @@ public class HttpServer implements Runnable{
                 e.printStackTrace();
             }
         }
-
-
-
     }
 }
