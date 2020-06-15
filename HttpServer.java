c271591b001a84b0decbd6e41e656be70da240cc
icimence <icimence@outlook.com>
2020-06-15 17:30:44 +0800

    feat: can start browser automatically

--- HttpServer.java
+++ HttpServer.java
@@ -31,6 +31,24 @@ public class HttpServer implements Runnable {
             serverSocket = new ServerSocket(port);
             System.out.println("Server is listening for a connection on port: " + port + "......");
             System.out.println("Visit http://localhost:8080 to have a try");
+            //判断当前系统是否支持Java AWT Desktop扩展
+            if(java.awt.Desktop.isDesktopSupported()){
+                try{
+                    //创建一个URI实例,注意不是URL
+                    java.net.URI uri=java.net.URI.create("http://localhost:8080");
+                    //获取当前系统桌面扩展
+                    java.awt.Desktop dp=java.awt.Desktop.getDesktop();
+                    //判断系统桌面是否支持要执行的功能
+                    if(dp.isSupported(java.awt.Desktop.Action.BROWSE)){
+                        //获取系统默认浏览器打开链接
+                        dp.browse(uri);
+                    }
+                }catch(java.lang.NullPointerException e){
+                    //此为uri为空时抛出异常
+                }catch(java.io.IOException e){
+                    //此为无法获取系统默认浏览器
+                }
+            }
             while (true) {//通过死循环建立长连接，不断监听客户端传来的消息
                 Socket client = serverSocket.accept();
 
